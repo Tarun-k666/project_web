79d426c3eb90097d81cc4cb7f7c350763ca9703e
Tarun Kumar <63114033+Tarun-k666@users.noreply.github.com>
2021-05-12 14:46:20 +0530

    Add files via upload

---
+++ web_developement1/assignments/hackathon_set2_3.py
@@ -0,0 +1,30 @@
+def check(arr,K,N):
+    flag=0
+    for i in range(N-K):
+
+        if flag==0:
+            if arr[i]%2!=0:
+                #print(arr[i],'odd')
+                for j in range(i+1,i+K):
+                    if arr[j]%2!=0:
+                        flag=1
+                        #print(arr[j],'odd')
+                    else:
+                        flag=0
+                        break
+                #print(flag)
+        else:
+            return 'yes'
+    return 'no'
+    
+
+
+T=int(input())
+result=[]
+for i in range(T):
+    N,K=map(int,input().split())
+    lst=list(map(int,input().split()))
+    lst.sort()
+    result.append(check(lst,K,N))
+for char in result:
+    print(char)
